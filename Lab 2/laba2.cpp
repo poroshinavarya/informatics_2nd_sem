56be34d18691c71b30802a056946bbd516501195
poroshinavarya <159236869+poroshinavarya@users.noreply.github.com>
2024-03-20 01:15:41 +0300

    Update laba2.cpp

--- Lab 2/laba2.cpp
+++ Lab 2/laba2.cpp
@@ -4,23 +4,80 @@
 using namespace std;
 
 class Vehicle{
-
-
-
-public:
     int WheelCnt;//количество колёс
     double mileage;//пробег в км/всё время
     double Volume;//объём бака в л
-    double speed;
-    double Power;//мощность двигателя
-    double fuelCons;//расход топлива
     double t;//время в пути
     char name[40];
-    //public:
-    double Speedfunc(double Power, int cnt);
-    double Fuelfunc(double t);
+    double Power;//мощность двигателя
+public:
+    double speed;
+    double fuelCons;//расход топлива
+
     Vehicle();
     ~Vehicle();
+
+   // void input(int countVehicles, Vehicle *pVehicles);
+   void input(int countVehicles, Vehicle *pVehicles){
+  for(int i=0;i<countVehicles;i++){
+    cout << "Enter the name of the vehicle: ";
+    if(!(cin >> pVehicles[i].name[40])){
+        cout<<"error"<<endl;
+        return;
+    };
+    cout << "Enter the count of wheels: ";
+    cin >> pVehicles[i].WheelCnt;
+    cout << "Enter the mileage: ";
+    cin >> pVehicles[i].mileage;
+    cout << "Enter the volume of the tank: ";
+    cin >> pVehicles[i].Volume;
+    cout << "Enter the power: ";
+    cin >> pVehicles[i].Power;
+  }
+}
+    void Speedfunc(int countVehicles, Vehicle *pVehicles);
+    void Fuelfunc(int countVehicles, Vehicle *pVehicles);
+    void output(int countVehicles, Vehicle *pVehicles);
+    void trackTime_refuelings(int routeLength, Vehicle *pVehicles, int countVehicles);
+    void Set_name(char nameCar[40]){
+        strcpy(name,nameCar);
+    }
+    char get_name(){
+        for(int i=0;i<40;i++){
+            cout<<name[i];
+        }
+        return 0;
+    }
+    void set_WheelCnt(int Wheel){
+        this->WheelCnt=Wheel;
+    }
+    int get_WheelCnt(){
+        return WheelCnt;
+    }
+    void set_mileage(double mile){
+        this->mileage=mile;
+    }
+    double get_mileage(){
+        return mileage;
+    }
+    void set_Volume(double vol){
+        this->Volume=vol;
+    }
+    double get_Volume(){
+        return Volume;
+    }
+    void set_Power(double power){
+        this->Power=power;
+    }
+    double get_Power(){
+        return Power;
+    }
+    void set_Time(double time){
+        this->t=time;
+    }
+    double get_Time(){
+        return t;
+    }
 };
 
 
@@ -36,41 +93,43 @@ Vehicle::~Vehicle(){
 //    delete Vehicle;
 }
 
-void input(int countVehicles, Vehicle *pVehicles)
-{
-  for(int i=0;i<countVehicles;i++)
-  {
-    cout << "Enter the name of the vehicle: ";
-    if(!(cin >> pVehicles[i].name)){
-        cout<<"error"<<endl;
-        return;
-    };
-    cout << "Enter the count of wheels: ";
-    cin >> pVehicles[i].WheelCnt;
-    cout << "Enter the mileage: ";
-    cin >> pVehicles[i].mileage;
-    cout << "Enter the volume of the tank: ";
-    cin >> pVehicles[i].Volume;
-    cout << "Enter the power: ";
-    cin >> pVehicles[i].Power;
-  }
+
+
+
+
+
+
+
+void output(int countVehicles, Vehicle *pVehicles){
+    for(int i=0;i<countVehicles;i++){
+      cout<<"name: "<<pVehicles[i].get_name()<<endl;
+      cout<<"count of wheels: "<<pVehicles[i].get_WheelCnt()<<endl;
+      cout<<"mileage: "<<pVehicles[i].get_mileage()<<endl;
+      cout<<"speed: "<<pVehicles[i].speed<<endl;
+      cout<<"volume of tank: "<<pVehicles[i].get_Volume()<<endl;
+      cout<<"fuel consumption: "<<pVehicles[i].fuelCons<<endl;
+      cout<<"power: "<<pVehicles[i].get_Power()<<endl;
+      cout<<"travel time: "<<pVehicles[i].get_Time()<<endl;
+    }
 }
-void Speedfunc(int countVehicles, Vehicle *pVehicles){
+
+
+void Vehicle::Speedfunc(int countVehicles, Vehicle *pVehicles){
     for(int i=0;i<countVehicles;i++){
         pVehicles[i].speed=sqrt(pVehicles[i].Power)*((70/pVehicles[i].WheelCnt)-2.5);
     }
 }
-void Fuelfunc(int countVehicles, Vehicle *pVehicles){
+void Vehicle::Fuelfunc(int countVehicles, Vehicle *pVehicles){
     for(int i=0;i<countVehicles;i++){
       pVehicles[i].fuelCons=(pow(pVehicles[i].Power,1/3)+sqrt(pVehicles[i].Power))-6.25;
     }
 }
-void output(int countVehicles, Vehicle *pVehicles){
+void Vehicle::output(int countVehicles, Vehicle *pVehicles){
   for(int i=0;i<countVehicles;i++){
       cout<<pVehicles[i].name<<": "<<pVehicles[i].WheelCnt<<"wheels, "<<pVehicles[i].Power<<"horsepower; "<<pVehicles[i].speed<<"km/h; "<<pVehicles[i].fuelCons<<"l/100km"<<endl;
     }
 }
-void trackTime_refuelings(int routeLength, Vehicle *pVehicles, int countVehicles){
+void Vehicle::trackTime_refuelings(int routeLength, Vehicle *pVehicles, int countVehicles){
     double time[countVehicles];
     int num_refuelings[countVehicles];
     char name_V[countVehicles][40];
@@ -87,30 +146,30 @@ void trackTime_refuelings(int routeLength, Vehicle *pVehicles, int countVehicles
     }
     for(int i=0;i<countVehicles;i++){
         for(int j=0;j<countVehicles-1;++j){
-//            if(time[j]>time[j+1]){
-//                double temp=time[j];
-//                time[j]=time[j+1];
-//                time[j+1]=temp;
-//                int temp_ref=num_refuelings[j];
-//                num_refuelings[j]=num_refuelings[j+1];
-//                num_refuelings[j+1]=temp_ref;
-//                char temp_name[40];
-//                strcpy(temp_name, name_V[j]);
-//                strcpy(name_V[j], name_V[j+1]);
-//                strcpy(name_V[j+1], temp_name);
-//            }
+            if(time[j]>time[j+1]){
+                double temp=time[j];
+                time[j]=time[j+1];
+                time[j+1]=temp;
+                int temp_ref=num_refuelings[j];
+                num_refuelings[j]=num_refuelings[j+1];
+                num_refuelings[j+1]=temp_ref;
+                char temp_name[40];
+                strcpy(temp_name, name_V[j]);
+                strcpy(name_V[j], name_V[j+1]);
+                strcpy(name_V[j+1], temp_name);
+            }
         if(time[j]==time[j+1]){
           if(num_refuelings[j]>num_refuelings[j+1]){
             int temp=time[j];
             time[j]=time[j+1];
             time[j+1]=temp;
-//            int temp_ref=num_refuelings[j];
-//            num_refuelings[j]=num_refuelings[j+1];
-//            num_refuelings[j+1]=temp_ref;
-//            char temp_name[30];
-//            strcpy(temp_name,name_V[j]);
-//            strcpy(name_V[j],name_V[j+1]);
-//            strcpy(name_V[j+1],temp_name);
+            int temp_ref=num_refuelings[j];
+            num_refuelings[j]=num_refuelings[j+1];
+            num_refuelings[j+1]=temp_ref;
+            char temp_name[30];
+            strcpy(temp_name,name_V[j]);
+            strcpy(name_V[j],name_V[j+1]);
+            strcpy(name_V[j+1],temp_name);
           }
         }
       }
@@ -135,7 +194,7 @@ void new_page()
 }
 
 
-void menu(int countVehicles, Vehicle *pVehicles,int routeLength){
+void menu(int countVehicles, Vehicle *pVehicles, int routeLength){
     int exit=0;
     int complete=0;
     int cur=0;
@@ -155,21 +214,51 @@ void menu(int countVehicles, Vehicle *pVehicles,int routeLength){
     }
     switch(choice){
 case 1:
-//    cout << "Enter "<<endl;
-//    new_page();
-//      input(countVehicles, pVehicles);
-//      Speedfunc(countVehicles,pVehicles);
-//      Fuelfunc(countVehicles, pVehicles);
-
+    new_page();
+    //input(countVehicles, pVehicles);
+    for(int i=cur-1;i<cur;++i){
+            pVehicles[i].input(countVehicles, pVehicles);
+        pVehicles[i].Fuelfunc(countVehicles, pVehicles);
+        pVehicles[i].Speedfunc(countVehicles, pVehicles);
+    }
+    cur++;
     break;
 case 2:
-    break;
+    new_page();
+      if(cur==0){
+        cout<<"you need to input vehicle!\n";
+        break;
+      }
+      output(countVehicles, pVehicles);
+      break;
+    case 3:
+      new_page();
+      if(cur==0){
+        cout<<"you need to input vehicle!\n";
+        break;
+      }
+      cout<<"Enter the lenght of the track: ";
+      cin>>routeLength;
 
+      break;
+    case 4:
+      if(routeLength==0){
+        cout<<"ENTER LENGHT OF THE TRACK , before determinate track!"<<"\n";
+        break;
+      }
+      new_page();
+      pVehicles[0].trackTime_refuelings(routeLength, pVehicles, countVehicles);
+      complete++;
+      break;
 
-default:
-    cout << "Error "<<endl;
-    break;
-
+    case 5:
+      new_page();
+      cout<<"Are you sure to exit? 1-yes 0-no\n";
+      cin>>exit;
+      if(exit==1){
+        return;
+      }
+      break;
     }
   }
 }
